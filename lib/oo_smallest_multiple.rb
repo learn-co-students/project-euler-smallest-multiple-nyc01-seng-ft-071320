baaa1d86cfd10861667830bb9004d2294fe969d3
humorous-storage-7521 <rmwerner@umich.edu>
2021-02-16 15:48:51 -0600

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+
+    def initialize(num)
+        @num = num
+    end
+
+    def verify_factors(big_num)
+        non_divisible_factors = (1..@num).filter {|n| big_num % n != 0 }
+        return non_divisible_factors == []
+    end
+
+    def lcm
+        multiple = 0
+        found_answer = false 
+        while found_answer == false
+            multiple += @num
+            found_answer = verify_factors(multiple)
+        end
+        return multiple
+    end 
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,16 @@
 # Implement your procedural solution here!
+
+def verify_factors(big_num, num_of_factors)
+    non_divisible_factors = (1..num_of_factors).filter {|n| big_num % n != 0 }
+    return non_divisible_factors == []
+end
+
+def smallest_multiple(num)
+    multiple = 0
+    found_answer = false 
+    while found_answer == false
+        multiple += num
+        found_answer = verify_factors(multiple, num)
+    end
+    return multiple
+end 
